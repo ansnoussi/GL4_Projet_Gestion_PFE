7db8860096fb541f8ffea8b55c68bc4d6654cabb
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-12-06 20:36:24 +0100

    first commit to the backend

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@gl4/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!' };
+    return { message: 'Welcome to backend!' };
   }
 }
