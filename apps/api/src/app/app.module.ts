87df2a69988ff2681d06b47b13cbdfd4d129339d
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-12-06 22:46:15 +0100

    controllers structure

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntrepriseController } from './controllers/entreprise/entreprise.controller';
+import { EtudiantController } from './controllers/etudiant/etudiant.controller';
+import { EnseignantController } from './controllers/enseignant/enseignant.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EntrepriseController, EtudiantController, EnseignantController],
   providers: [AppService],
 })
 export class AppModule {}
