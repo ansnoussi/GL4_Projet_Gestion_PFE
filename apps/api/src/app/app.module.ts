13120f3cde0a7da8c8f4c8c5882c72205f39dbd5
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-12-07 14:50:15 +0100

    mongo but not secure

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EntrepriseController } from './controllers/entreprise/entreprise.controller';
 import { EtudiantController } from './controllers/etudiant/etudiant.controller';
 import { EnseignantController } from './controllers/enseignant/enseignant.controller';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://gl4user:5CMsEASbsCmtuD4f@cluster0.awyx9.mongodb.net/projet-gl4?retryWrites=true&w=majority')],
   controllers: [AppController, EntrepriseController, EtudiantController, EnseignantController],
   providers: [AppService],
 })
