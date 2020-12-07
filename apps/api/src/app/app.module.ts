bdc88bc5d0b204345b35866b94e680feef89da44
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-12-07 14:56:36 +0100

    better config management

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import mongoUrl from './config/db'
 import { EntrepriseController } from './controllers/entreprise/entreprise.controller';
 import { EtudiantController } from './controllers/etudiant/etudiant.controller';
 import { EnseignantController } from './controllers/enseignant/enseignant.controller';
 
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://gl4user:5CMsEASbsCmtuD4f@cluster0.awyx9.mongodb.net/projet-gl4?retryWrites=true&w=majority')],
+  imports: [MongooseModule.forRoot(mongoUrl)],
   controllers: [AppController, EntrepriseController, EtudiantController, EnseignantController],
   providers: [AppService],
 })
---
+++ apps/api/src/app/config/db.ts
@@ -0,0 +1,3 @@
+const mongoUrl = 'mongodb://gl4user:5CMsEASbsCmtuD4f@cluster0.awyx9.mongodb.net/projet-gl4?retryWrites=true&w=majority';
+
+export default mongoUrl;
