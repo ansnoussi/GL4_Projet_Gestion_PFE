5a6780ab959f83b10bfcba564aafc47fdc1deb93
Mohamed Lamine Barghouda <laminebarghouda@gmail.com>
2021-01-18 12:23:36 +0100

    setting sidemenu show option

--- apps/pfe/src/app/components/navigation/header/header.component.ts
+++ apps/pfe/src/app/components/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() public showSideMenu;
   logout(){
     this.router.navigateByUrl('login');
   }
