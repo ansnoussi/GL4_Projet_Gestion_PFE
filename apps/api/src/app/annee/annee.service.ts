81e640930e138bacc651ac1d8785358e8f6d7ae4
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-01-21 02:45:01 +0100

    created the year service

--- apps/api/src/app/annee/annee.service.ts
+++ apps/api/src/app/annee/annee.service.ts
@@ -5,6 +5,15 @@ import { IAnnee } from './interfaces/annee.interface';
 
 @Injectable()
 export class AnneeService {
-  constructor(@InjectModel('User') private anneeModel: Model<IAnnee>) { }
+  constructor(@InjectModel('Annee') private anneeModel: Model<IAnnee>) { }
 
+  async advanceYear(year): Promise<IAnnee> {
+    await  this.anneeModel.findOneAndUpdate({ isCurrent : true }, {isCurrent : false}):
+    let newYear = new this.anneeModel({year : year , isCurrent : true}):
+    return await newYear.save();
+  }
+
+  async getCurrent(): Promise<IAnnee> {
+    return await  this.anneeModel.findOne({ isCurrent : true });
+  }
 }
--- apps/api/src/app/annee/schemas/annee.schema.ts
+++ apps/api/src/app/annee/schemas/annee.schema.ts
@@ -1,6 +1,12 @@
 import * as mongoose from 'mongoose';
 
 export const AnneeSchema = new mongoose.Schema({
-
+    year: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    isCurrent: {
+        type: Boolean
+    }
 },{timestamps:true});
-
